6ef48e181067abc9b3b2d16108ccaa6cc8e2dca8
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-12-18 23:21:13 +0700

    Delete backend/routes/crowdfundRoutes.js

--- backend/routes/crowdfundRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllCrowdfunds } = require('../controllers/crowdfundController');
-
-const router = express.Router();
-
-router.get('/', getAllCrowdfunds);
-
-module.exports = router;
