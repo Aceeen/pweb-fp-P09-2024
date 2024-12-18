44c973c45586a98e0511869c5802cea1f51b5c9d
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-12-19 00:03:53 +0700

    Delete backend/utils directory

--- backend/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    const currentTime = new Date().toISOString();
-    console.log(`[${currentTime}] ${req.method} ${req.originalUrl}`);
-    next();
-  };
-  
-  module.exports = logger;
-  
