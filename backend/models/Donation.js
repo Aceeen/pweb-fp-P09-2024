e78dde3f4ba1e55c11f775344db614b5d54b1d49
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-12-18 23:20:56 +0700

    Delete backend/models/Donation.js

--- backend/models/Donation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DonationSchema = new mongoose.Schema({
-  payment_method: { type: String, enum: ['QRIS', 'BANK_TRANSFER'], required: true },
-  amount: { type: Number, required: true },
-  created_at: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Donation', DonationSchema);
