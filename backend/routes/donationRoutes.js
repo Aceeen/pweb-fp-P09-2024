6f7f0995712fcd2063bf8e870f75b0cff98b82ff
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-12-18 23:21:20 +0700

    Delete backend/routes/donationRoutes.js

--- backend/routes/donationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { submitDonation } = require('../controllers/donationController');
-
-const router = express.Router();
-
-router.post('/', submitDonation);
-
-module.exports = router;
