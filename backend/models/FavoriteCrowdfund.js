9d3afbf2f92b25007fc16a6909a3f91e304d90d5
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-12-18 23:21:03 +0700

    Delete backend/models/FavoriteCrowdfund.js

--- backend/models/FavoriteCrowdfund.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FavoriteCrowdfundSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  crowdfund_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Crowdfund', required: true },
-});
-
-module.exports = mongoose.model('FavoriteCrowdfund', FavoriteCrowdfundSchema);
